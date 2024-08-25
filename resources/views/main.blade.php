bc3c115066b6b7bb155b6488edef75f21fda8997
Daniel Mihai <danikmihai23@mail.ru>
2024-08-25 17:35:49 +0300

    Initial Commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,12 @@
+<!-- Подключение шаблонизатора -->
+@extends("layouts.app")
+
+<!-- Динамическое название страницы -->
+@section("title")
+    Car Rent | RentoCar
+@endsection
+
+<!-- Подключние секции -->
+@section("main")
+
+@endsection
