24df34e355e1b37449a90008d7fffe2f2deeaf57
Daniel Mihai <danikmihai23@mail.ru>
2024-08-26 18:12:44 +0300

    Add header block
    Add header block for every page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Подключение CSS -->
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
     <title>@yield("title")</title>
 </head>
 <body>
+    <!-- Подключение футера сайта -->
+    @include("blocks.header")
+
     <!-- Подключение секций -->
     @yield("main")
 </body>
